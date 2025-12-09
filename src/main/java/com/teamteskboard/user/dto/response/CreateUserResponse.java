5ee53db60749961a530371c5e946fed3e7b6313a
Kingyul Kim <youremail@example.com>
2025-12-09 18:28:55 +0900

    Refactor: CreateUserResponse 속성 추가 / 👑
    userName 속성 추가

--- src/main/java/com/teamteskboard/user/dto/response/CreateUserResponse.java
+++ src/main/java/com/teamteskboard/user/dto/response/CreateUserResponse.java
@@ -10,13 +10,15 @@ import java.time.LocalDateTime;
 public class CreateUserResponse {
     private final Long id;
     private final String name;
+    private final String userName;
     private final String email;
     private final String role;
     private final LocalDateTime createdAt;
 
-    public CreateUserResponse(Long id, String name, String email, String role, LocalDateTime createdAt) {
+    public CreateUserResponse(Long id, String name, String userName, String email, String role, LocalDateTime createdAt) {
         this.id = id;
         this.name = name;
+        this.userName = userName;
         this.email = email;
         this.role = role;
         this.createdAt = createdAt;
@@ -26,6 +28,7 @@ public class CreateUserResponse {
         return new CreateUserResponse(
                 user.getId(),
                 user.getName(),
+                user.getUserName(),
                 user.getEmail(),
                 user.getRole().name(),
                 user.getCreatedAt()

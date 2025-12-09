8fd13becf3b9e95832bcf738c4217b98c384b4d3
Kingyul Kim <youremail@example.com>
2025-12-09 21:16:56 +0900

    Refactor: 이메일 정규식 수정 / 👑

--- src/main/java/com/teamteskboard/common/regexp/RegExp.java
+++ src/main/java/com/teamteskboard/common/regexp/RegExp.java
@@ -4,5 +4,5 @@ public class RegExp {
     public static final String NAME = "^[가-힣a-zA-Z]+$";
     public static final String USERNAME = "^[a-z][a-z0-9]*$";
     public static final String PASSWORD = "^[A-Za-z0-9!@#$%]+$";
-    public static final String EMAIL = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\\.[A-Za-z]{2,63}$";
+    public static final String EMAIL = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,63}$";
 }

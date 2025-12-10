cd2b7a8aa78d9a00b16432fe74e60ea20d9934b9
Kingyul Kim <youremail@example.com>
2025-12-10 09:46:37 +0900

    Remove: UpdateUserResponse 초기 생성 / 👑

---
+++ src/main/java/com/teamteskboard/user/dto/response/UpdateUserResponse.java
@@ -0,0 +1,39 @@
+package com.teamteskboard.user.dto.response;
+
+import com.teamteskboard.user.entity.User;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class UpdateUserResponse {
+    private final Long id;
+    private final String userName;
+    private final String email;
+    private final String name;
+    private final String role;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
+
+    public UpdateUserResponse(Long id, String userName, String email, String name, String role, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.name = name;
+        this.role = role;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
+
+    public static UpdateUserResponse from(User user) {
+        return new UpdateUserResponse(
+                user.getId(),
+                user.getUserName(),
+                user.getEmail(),
+                user.getName(),
+                user.getRole().name(),
+                user.getCreatedAt(),
+                user.getModifiedAt()
+        );
+    }
+}

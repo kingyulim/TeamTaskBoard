80b16341585cd4f989a8c9922212e7ff95ef12c5
jangse0 <syjang1016@korea.ac.kr>
2025-12-11 15:42:26 +0900

    Refactor: 페이징 방식 수정 / jsy

--- src/main/java/com/teamteskboard/task/service/TaskService.java
+++ src/main/java/com/teamteskboard/task/service/TaskService.java
@@ -15,9 +15,7 @@ import com.teamteskboard.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -60,22 +58,16 @@ public class TaskService {
     // 전체 조회
     @Transactional(readOnly = true)
     public Page<GetTaskResponse> getAllTasks(
-            int page,
-            int size,
             TaskStatusEnum status,
             String search,
-            Long assigneeId
+            Long assigneeId,
+            Pageable pageable
     ) {
+        Page<Task> tasks = taskRepository.findTasks(status, assigneeId, search, pageable);
 
-        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "modifiedAt"));
-
-        Page<Task> tasks = taskRepository.findTasks(status, assigneeId, search,pageable);
-
-        Page<GetTaskResponse> response = tasks.map(GetTaskResponse::from);
-
-        return response;
-
+        return tasks.map(GetTaskResponse::from);
     }
+
 
     // 작업 수정
     @Transactional

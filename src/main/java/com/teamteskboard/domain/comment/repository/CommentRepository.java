c653c7ecdcc9a4d7571d7f0ccee4eb4726337e70
Leechungwoon <dlcjddns22@gmail.com>
2025-12-12 14:28:52 +0900

    Refactor: CommentRepository 주석 변경  /LCW

--- src/main/java/com/teamteskboard/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/teamteskboard/domain/comment/repository/CommentRepository.java
@@ -1,6 +1,5 @@
-package com.teamteskboard.comment.repository;
+package com.teamteskboard.domain.comment.repository;
 
-import com.teamteskboard.comment.entity.Comment;
 import com.teamteskboard.common.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,10 +11,13 @@ import java.util.Optional;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 
+    //조회에서 사용
     Page<Comment> findByTaskIdAndIsDeletedFalse(Long taskId, Pageable pageable);
 
+    //수정에서 사용
     Optional<Comment> findByIdAndTaskIdAndIsDeletedFalse(Long id, Long taskId);
 
+    //생성 및 삭제에서 사용
     Optional<Comment> findByIdAndIsDeletedFalse(Long parentId);
 
     //부모댓글

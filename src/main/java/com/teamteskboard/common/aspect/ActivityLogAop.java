f3d4dfeac765b3d711ffe6b1e5dbfcee8de06ea3
minss22 <kms141516@gmail.com>
2025-12-12 10:52:13 +0900

    Chore: 사용하지 않는 import 삭제 / ms

--- src/main/java/com/teamteskboard/common/aspect/ActivityLogAop.java
+++ src/main/java/com/teamteskboard/common/aspect/ActivityLogAop.java
@@ -19,7 +19,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;

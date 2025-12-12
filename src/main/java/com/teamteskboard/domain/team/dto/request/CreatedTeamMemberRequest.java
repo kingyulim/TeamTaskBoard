bf238d045fa09f9094d872e0946914fc4cddc4b5
이용준 <abc157898@gmail.com>
2025-12-12 21:29:56 +0900

    Fix : 팀 멤버 생성 시 오류 해결 / LYJ - 요청 DTO Valid 어노테이션 NotBlank -> NotNull로 변경

--- src/main/java/com/teamteskboard/domain/team/dto/request/CreatedTeamMemberRequest.java
+++ src/main/java/com/teamteskboard/domain/team/dto/request/CreatedTeamMemberRequest.java
@@ -1,11 +1,12 @@
 package com.teamteskboard.domain.team.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class CreatedTeamMemberRequest {
 
-    @NotBlank(message = "추가할 멤버 ID를 입력해 주세요.")
+    @NotNull(message = "추가할 멤버 ID를 입력해 주세요.")
     private Long userId;
 }
